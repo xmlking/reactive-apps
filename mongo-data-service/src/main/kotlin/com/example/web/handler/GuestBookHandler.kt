ffae85d4fb3d3b4921aa60693bafc53db01c92bf
xmlking <xmlking@gmail.com>
2017-05-29 23:37:03 -0700

    ✨ Cleanup

--- mongo-data-service/src/main/kotlin/com/example/web/handler/GuestBookHandler.kt
+++ mongo-data-service/src/main/kotlin/com/example/web/handler/GuestBookHandler.kt
@@ -3,7 +3,7 @@ package com.example.web.handler
 import com.example.domain.GuestBookEntry
 import com.example.repository.GuestBookRepository
 import com.example.util.json
-import org.springframework.http.MediaType
+import org.springframework.http.MediaType.*
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse.ok
@@ -21,10 +21,10 @@ class GuestBookHandler(val repository: GuestBookRepository) {
     fun create(req: ServerRequest) = ok().json().body(repository.save(req.bodyToMono<GuestBookEntry>()))
 
     fun fetchSSE(req: ServerRequest) = ok()
-            .contentType(MediaType.TEXT_EVENT_STREAM)
+            .contentType(TEXT_EVENT_STREAM)
             .body(repository.tailByTimestampGreaterThan((GregorianCalendar.getInstance().time)), GuestBookEntry::class.java)
 
     fun fetch(req: ServerRequest) = ok()
-            .contentType(MediaType.APPLICATION_STREAM_JSON)
+            .contentType(APPLICATION_STREAM_JSON)
             .body(repository.tailByTimestampGreaterThan((GregorianCalendar.getInstance().time)), GuestBookEntry::class.java)
 }
