ceab8bc2123eda29e147c814afacac12f7d79360
Sumanth Chinthagunta <xmlking+github@gmail.com>
2017-09-10 09:01:17 -0700

    🐛 Fix https://github.com/gradle/kotlin-dsl/issues/488

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,14 +3,8 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 // Prevents common subproject dependencies from being included in the common jar itself.
 // Without this, each subproject that included common would include each shared dependency twice.
-tasks {
-    withType<BootJar> {
-        enabled = false
-        classifier = "boot"
-        mainClass = "com.example.GuestBookEntryDTO"
-    }
-    withType<Jar> {
-        enabled = true
-    }
-}
+val jar: Jar by tasks
+val bootJar: BootJar by tasks
 
+bootJar.enabled = false
+jar.enabled = true
